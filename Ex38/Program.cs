a7f8847c2ed447026b6bc5db4f4399faed464825
Samantha <samantha@mail.com>
2022-11-04 16:39:52 +0300

    add programme Ex38

---
+++ Ex38/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+
+try
+{
+
+Console.Write("Укажите размер массива (введите натуральное число): ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+
+void FillArray(double[] arr)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        arr[index] = Convert.ToDouble (new Random().Next(-1000,1001)) / 10;
+        index++;
+    }
+}
+void PrintArray(double[] arr)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(array);
+PrintArray(array);
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+
+Console.WriteLine($"Максимальный элемент массива: {max}, минимальный элемент массива: {min}, разница между максимальным и минимальным элементами составляет {max - min}.");
+
+}
+
+catch
+{
+    Console.WriteLine("Ошибка! Введите натуральное число.");
+}
